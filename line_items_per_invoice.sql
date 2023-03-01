02a8c64b601dd13a6724fd073fb51695c9f402c5
Lauren Hanson <lohanson33@gmail.com>
2023-03-01 15:11:59 -0600

    invoices with line item count

--- line_items_per_invoice.sql
+++ line_items_per_invoice.sql
@@ -1,6 +1,6 @@
 SELECT 
-    i.InvoiceId,
-    COUNT(il.InvoiceLineId) AS Line_Items_Per_Invoice
+    i.*,
+    COUNT(il.InvoiceLineId) AS LineItemsPerInvoice
 FROM Invoice i 
 JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
 GROUP BY i.InvoiceId
