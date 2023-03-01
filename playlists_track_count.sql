29a06cbe04073c161f2f05c661226113af0bd285
Lauren Hanson <lohanson33@gmail.com>
2023-03-01 14:52:09 -0600

    LEFT JOIN added to playlist count

--- playlists_track_count.sql
+++ playlists_track_count.sql
@@ -2,6 +2,6 @@ SELECT
     pl.name AS Playlist_Name,
     COUNT(t.TrackId) AS Number_of_Tracks 
 FROM Playlist pl
-JOIN PlaylistTrack pt ON pl.PlayListId = pt.PlayListId
-JOIN Track t ON pt.TrackId = t.TrackId
+LEFT JOIN PlaylistTrack pt ON pl.PlayListId = pt.PlayListId
+LEFT JOIN Track t ON pt.TrackId = t.TrackId
 GROUP BY pl.name
--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -2,3 +2,5 @@ SELECT
     COUNT(CASE WHEN InvoiceDate LIKE "%2009%" THEN 1 END) AS "Invoice_Count_2009", 
     COUNT(CASE WHEN InvoiceDate LIKE "%2011%" THEN 1 END) AS "Invoice_Count_2011"
 FROM Invoice
+
+
